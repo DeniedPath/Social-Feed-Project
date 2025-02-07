65926e623a9a16ff603da08be54bf7159b817913
Path <57870461+DeniedPath@users.noreply.github.com>
2025-02-07 07:58:34 -0500

    yes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure Tailwind scans all relevant files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
